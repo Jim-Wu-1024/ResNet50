666ac5b9216bc3f7731262a04e78b66a043f1ab1
Jim-Wu-1024 <jimwu.1024@gmail.com>
2024-09-08 00:39:48 -0400

    Implementation of ResNet50 Bottleneck Block

---
+++ resnet.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+
+from typing import Optional
+
+
+class Bottleneck(nn.Module):
+    # Expansion factor for the Bottleneck structure
+    expansion: int = 4
+
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 stride: int = 1,
+                 downsample: Optional[nn.Module] = None,
+                 ):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion,
+                               kernel_size=1, stride=stride, padding=0, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x  # Save input for the skip connection
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+
+        # Residual connection
+        x += identity
+        x = self.relu(x)
+        return x
+
+
+
+
+
